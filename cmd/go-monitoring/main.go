e88810bdaae94f909d7598d262a4ff0870023bf1
Pjotr <git.pjotr@pjotrs.nl>
2023-04-02 23:42:17 +0200

    base swagger api

--- cmd/go-monitoring/main.go
+++ cmd/go-monitoring/main.go
@@ -2,23 +2,82 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"log"
+	"net/http"
+	"os"
+	"strings"
 
+	"github.com/go-openapi/loads"
+	"github.com/jessevdk/go-flags"
 	"github.com/pjotrscholtze/go-monitoring/cmd/go-monitoring/checkmanager"
 	"github.com/pjotrscholtze/go-monitoring/cmd/go-monitoring/config"
 	"github.com/pjotrscholtze/go-monitoring/cmd/go-monitoring/entity"
 	"github.com/pjotrscholtze/go-monitoring/cmd/go-monitoring/informer"
 	"github.com/pjotrscholtze/go-monitoring/cmd/go-monitoring/repo"
+	"github.com/pjotrscholtze/go-monitoring/restapi"
+	"github.com/pjotrscholtze/go-monitoring/restapi/operations"
 )
 
 // "github.com/pjotrscholtze/go-buildserver/cmd/go-buildserver/process"
 
+//	func main() {
+//		// process.StartProcessWithStdErrStdOutCallback("/bin/sh",
+//		// 	[]string{path.Join("/home/pjotr/go/src/github.com/pjotrscholtze/go-monitoring", "boot.sh")},
+//		// 	func(pt process.PipeType, t time.Time, s string) {
+//		// 		println(s)
+//		// 	})
+//		cui := informer.NewCheckUpdateInformer()
+//		tcr := repo.NewTargetCheckRepoInMemory(5)
+//		cui.RegisterListenerFunc(func(result entity.Result, target config.Target, check config.Check) {
+//			tcr.UpdateCheck(result, target, check)
+//			fmt.Printf("via informer, we got %s, %s, %s\n", result.Message(), target.Name, check.Name)
+//		})
+//		cm := checkmanager.NewCheckManager(config.LoadMockConfig(), cui)
+//		cm.ValidateConfig()
+//		cm.Run()
+//		// err := cm.PerformAllChecks()
+//		// if err != nil {
+//		// 	log.Fatal(err.Error())
+//		// }
+//		for {
+//			time.Sleep(time.Second)
+//			for _, tce := range tcr.List() {
+//				println(" from main")
+//				tce.Result.Log()
+//			}
+//		}
+//	}
+type mock struct {
+	next http.Handler
+	mux  *http.ServeMux
+}
+
+func (m *mock) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if strings.HasPrefix((*r).RequestURI, "/api/") ||
+		strings.HasPrefix((*r).RequestURI, "/swagger.json") {
+		m.next.ServeHTTP(w, r)
+		return
+	}
+	m.mux.ServeHTTP(w, r)
+}
+
 func main() {
-	// process.StartProcessWithStdErrStdOutCallback("/bin/sh",
-	// 	[]string{path.Join("/home/pjotr/go/src/github.com/pjotrscholtze/go-monitoring", "boot.sh")},
-	// 	func(pt process.PipeType, t time.Time, s string) {
-	// 		println(s)
-	// 	})
+	// if len(os.Args) != 2 {
+	// 	println("Usage: app <config-path.yaml>")
+	// 	return
+	// }
+	// path := os.Args[1]
+	log.Println("Starting Go monitoring server")
+
+	// log.Printf("Loading config: %s\n", path)
+
+	swaggerSpec, err := loads.Embedded(restapi.SwaggerJSON, restapi.FlatSwaggerJSON)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	api := operations.NewGoMonitoringAPI(swaggerSpec)
+	server := restapi.NewServer(api)
 	cui := informer.NewCheckUpdateInformer()
 	tcr := repo.NewTargetCheckRepoInMemory(5)
 	cui.RegisterListenerFunc(func(result entity.Result, target config.Target, check config.Check) {
@@ -28,15 +87,41 @@ func main() {
 	cm := checkmanager.NewCheckManager(config.LoadMockConfig(), cui)
 	cm.ValidateConfig()
 	cm.Run()
-	// err := cm.PerformAllChecks()
-	// if err != nil {
-	// 	log.Fatal(err.Error())
-	// }
-	for {
-		time.Sleep(time.Second)
-		for _, tce := range tcr.List() {
-			println(" from main")
-			tce.Result.Log()
+
+	defer server.Shutdown()
+
+	parser := flags.NewParser(server, flags.Default)
+	parser.ShortDescription = "Go Monitoring server"
+	parser.LongDescription = swaggerSpec.Spec().Info.Description
+	server.ConfigureFlags()
+	for _, optsGroup := range api.CommandLineOptionsGroups {
+		_, err := parser.AddGroup(optsGroup.ShortDescription, optsGroup.LongDescription, optsGroup.Options)
+		if err != nil {
+			log.Fatalln(err)
 		}
+	}
+
+	if _, err := parser.Parse(); err != nil {
+		code := 1
+		if fe, ok := err.(*flags.Error); ok {
+			if fe.Type == flags.ErrHelp {
+				code = 0
+			}
+		}
+		os.Exit(code)
+	}
+
+	server.ConfigureAPI()
+	server.Port = 3000
+	server.Host = "0.0.0.0"
+
+	// t := &mock{
+	// 	next: api.Serve(nil),
+	// 	mux:  controller.RegisterUIController(),
+	// }
+	// server.SetHandler(t)
+
+	if err := server.Serve(); err != nil {
+		log.Fatalln(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,32 @@ module github.com/pjotrscholtze/go-monitoring
 go 1.20
 
 require (
-	github.com/pjotrscholtze/go-buildserver v0.0.0-20230328161714-91a0681b823d // indirect
-	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/go-openapi/errors v0.20.3
+	github.com/go-openapi/loads v0.21.2
+	github.com/go-openapi/runtime v0.25.0
+	github.com/go-openapi/spec v0.20.8
+	github.com/go-openapi/strfmt v0.21.7
+	github.com/go-openapi/swag v0.22.3
+	github.com/go-openapi/validate v0.22.1
+	github.com/jessevdk/go-flags v1.5.0
+	github.com/pjotrscholtze/go-buildserver v0.0.0-20230328161714-91a0681b823d
+	github.com/robfig/cron/v3 v3.0.1
+	golang.org/x/net v0.8.0
+)
+
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
+	github.com/go-openapi/analysis v0.21.4 // indirect
+	github.com/go-openapi/jsonpointer v0.19.6 // indirect
+	github.com/go-openapi/jsonreference v0.20.2 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	go.mongodb.org/mongo-driver v1.11.3 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
